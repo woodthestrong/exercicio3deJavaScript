71af8aafc62482445e90e10e150a7585b343853c
Woody Pascal Jean louis <woodthestrong@gmail.com>
2022-06-24 19:55:57 -0300

    Exercicio 3 de JavaScrpt

---
+++ index.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    var nome = prompt("Digite o seu nome");
+    var sobrenome = prompt("Digite o seu sobrenome");
+    var idade = prompt("Digite a sua idade");
+    var sexo = prompt("Digite o seu sexo");
+    var nomeDoPai = prompt("Digite o nome do seu pai");
+    var nomeDaMae = prompt("Digite o nome da sua mãe");
+    console.log("Nome: " + nome);
+    console.log("Sobrenome: " + sobrenome);
+    console.log("Idade: " + idade);
+    console.log("Sexo: " + sexo);
+    console.log("Nome do pai: " + nomeDoPai);
+    console.log("Nome da mãe: " + nomeDaMae);
+}
