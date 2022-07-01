ef3c73fc65bb4141d816e6d97f364ee93c301172
Woody Pascal Jean louis <woodthestrong@gmail.com>
2022-07-01 20:49:19 -0300

    Exercício 2 de JavaScript

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ function myFunction(){
     var sexo = prompt("Digite o seu sexo");
     var nomeDoPai = prompt("Digite o nome do seu pai");
     var nomeDaMae = prompt("Digite o nome da sua mãe");
-    console.log("Nome: " + nome);
-    console.log("Sobrenome: " + sobrenome);
-    console.log("Idade: " + idade);
-    console.log("Sexo: " + sexo);
-    console.log("Nome do pai: " + nomeDoPai);
-    console.log("Nome da mãe: " + nomeDaMae);
+    var nomeDaSegundaPessoa = prompt("Digite o seu nome");
+    var sobrenomeDaSegundaPessoa = prompt("Digite o seu sobrenome");
+    var idadeDaSegundaPessoa = prompt("Digite a sua idade");
+    var sexoDaSegundaPessoa = prompt("Digite o seu sexo");
+    var nomeDoPaiDaSegundaPessoa = prompt("Digite o nome do seu pai");
+    var nomeDaMaeDaSegundaPessoa = prompt("Digite o nome da sua mãe");
     console.table([nome, sobrenome, idade, sexo, nomeDoPai, nomeDaMae]);
+    console.table([nomeDaSegundaPessoa, sobrenomeDaSegundaPessoa, idadeDaSegundaPessoa, sexoDaSegundaPessoa, nomeDoPaiDaSegundaPessoa, nomeDaMaeDaSegundaPessoa]);
+    if (idade > idadeDaSegundaPessoa){
+        console.log(nome + " é maior do que " + nomeDaSegundaPessoa);
+    } else {
+        console.log(nomeDaSegundaPessoa + " é maior do que " + nome);
+    }
 }
